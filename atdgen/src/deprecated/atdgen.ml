f1664c8bf360231981525f393acb1a7a2256cb32
Raman Varabets <raman+git@ahrefs.com>
2024-09-03 09:23:36 +0200

    Rename all atdgen-www to atdgen-extra

--- atdgen/src/deprecated/atdgen.ml
+++ atdgen/src/deprecated/atdgen.ml
@@ -1,3 +1,3 @@
-[@@@deprecated "The runtime for Atdgen-www is now the module Atdgen_www_runtime existing
-in the atdgen-www-runtime opam package"]
-include Atdgen_www_runtime
+[@@@deprecated "The runtime for Atdgen-extra is now the module Atdgen_extra_runtime existing
+in the atdgen-extra-runtime opam package"]
+include Atdgen_extra_runtime
